dd4171a1a5a6e97fdd943e2826eb8490863c6168
quzor <egor.khantakov@gmail.com>
2025-07-12 12:25:12 +0700

    Реализована простая идея субординатора (stable_distribution), но долгое время работы и из-за этого долгое время работы monte_carlo

---
+++ code/__init__.py
@@ -0,0 +1,4 @@
+from . import finmath1
+from . import stable_distribution
+
+__all__ = ["finmath1", "stable_distribution"]
---
+++ code/stable_distribution/__init__.py
@@ -0,0 +1,7 @@
+from .sampling import sample_s_distribution, compute_first_crossing, plot_subordinator
+
+__all__ = [
+    "sample_s_distribution",
+    "compute_first_crossing",
+    "plot_subordinator",
+]
